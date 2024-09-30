0ac38aa9e7e82f48fad28258635c616686b5327b
george-coast <george.taylor@coastautonomous.com>
2024-09-30 17:07:00 -0400

    Update download_geoip.sh

--- download_geoip.sh
+++ download_geoip.sh
@@ -8,3 +8,7 @@ DESTINATION="/"
 
 # Download the file to the specified destination
 wget -P "$DESTINATION" "$URL"
+
+
+curl -L -o GeoLite2-City-CSV_20240927.zip "https://download.maxmind.com/app/geoip_download_by_token?date=20240927&edition_id=GeoLite2-City-CSV&suffix=zip&token=v2.local.vQfonZ0Vkiiwv_44iWvGLUceoz-_nnRWNqrkvzhuRrarHH4WfGlIeZqP-c77XsEhg2oWzmD0IoPpDj_SNwOlwg2mkG9bjmlPA-be9pFDgQh-fhDilP7Jl86ihtds_6iCmJ6zp4gPoMDoiDFlZQLIMnfrcMlQm2L-DK2SiBjoRo_55upToq2IlrGf69htJkTd5_3aI-g"
+
